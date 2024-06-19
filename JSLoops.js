f7a3e248031af15a1b6f85dd0a13c231e9eb207e
pushpakaran <mblanc581@gmail.com>
2024-06-19 21:42:33 +0530

    loops data

--- JSLoops.js
+++ JSLoops.js
@@ -141,5 +141,38 @@ numbers.forEach(myFunction);
 function myFunction(value) {
   txt += value;
 }
+*/
+
+
+/*JavaScript For Of - The For Of Loop
+The JavaScript for of statement loops through the values of an iterable object.
+It lets you loop over iterable data structures such as Arrays, Strings, Maps, NodeLists, and more:
+Syntax
+for (variable of iterable) {
+  // code block to be executed
+}
+
+variable - For every iteration the value of the next property is assigned to the variable. Variable can be declared with const, let, or var.
+iterable - An object that has iterable properties.
+
+Looping over an Array
+Example
+const cars = ["BMW", "Volvo", "Mini"];
+let text = "";
+for (let x of cars) {
+  text += x;
+}
+
+Looping over a String
+Example
+let language = "JavaScript";
+let text = "";
+for (let x of language) {
+text += x;
+}
+*/
+
+
+/* JavaScript While Loop - https://www.w3schools.com/js/js_loop_while.asp
 
 */
